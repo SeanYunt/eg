cf405f93ddaea4974f8fdf607e51103eae4620f7
Sean Yunt <syunt@carrentals.com>
2018-05-22 15:11:51 -0700

    duration logging

--- Q3/test/test_remove_word.js
+++ Q3/test/test_remove_word.js
@@ -4,27 +4,37 @@ var assert = require('assert');
 describe('remove word from sentence', function() {
   describe('happy path', function() {
     it('simple case', function() {
+      console.time('duration');
       assert.equal(func.removeWordFromSentence("Hello big world", "big"), "Hello  world");
+      console.timeEnd('duration');
     });
   });
   describe('edge case', function() {
     it('longer with punctuation', function() {
+      console.time('duration');
       assert.equal(func.removeWordFromSentence("To determine a floodplain boundary, we first identify a “storm event” that concerns us. ", "floodplain"), "To determine a  boundary, we first identify a “storm event” that concerns us. ");
+      console.timeEnd('duration');
     });
   });
   describe('edge case', function() {
     it('single word', function() {
+      console.time('duration');
       assert.equal(func.removeWordFromSentence("Go.", "Go"), ".");
+      console.timeEnd('duration');
     });
   });  
   describe('i18n', function() {
     it('traditional Chinese', function() {
+      console.time('duration');
       assert.equal(func.removeWordFromSentence("我不餓。", "不"), "我餓。");
+      console.timeEnd('duration');
     });
   });
   describe('i18n', function() {
     it('French', function() {
+      console.time('duration');
       assert.equal(func.removeWordFromSentence("Je n'ai pas faim.", "pas "), "Je n'ai faim.");
+      console.timeEnd('duration');
     });
   });
 });
